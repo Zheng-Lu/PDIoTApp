6dbc84b4a6db6959ba341a8df21e50fac09df12d
Shu Gu <abshugu@outlook.com>
2023-10-17 21:41:21 +0100

    First update of login/register/forget page

---
+++ app/build/generated/source/buildConfig/debug/com/specknet/pdiotapp/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.specknet.pdiotapp;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.specknet.pdiotapp";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
