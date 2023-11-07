1bd14eb24101b8a8932ce02d05f51b79a61a49dd
Shu Gu <abshugu@outlook.com>
2023-11-07 22:42:36 +0000

    UI fix, now recog and preview works in same page

--- app/src/main/java/com/specknet/pdiotapp/recognition/RecogniseActivity.kt
+++ app/src/main/java/com/specknet/pdiotapp/recognition/RecogniseActivity.kt
@@ -22,6 +22,12 @@ import java.nio.channels.FileChannel
 import java.text.SimpleDateFormat
 import java.util.*
 
+import androidx.core.content.ContextCompat
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
 
 class RecogniseActivity : AppCompatActivity() {
 
@@ -79,12 +85,35 @@ class RecogniseActivity : AppCompatActivity() {
     private val filterTestRespeck = IntentFilter(Constants.ACTION_RESPECK_LIVE_BROADCAST)
     private val filterTestThingy = IntentFilter(Constants.ACTION_THINGY_BROADCAST)
 
+
+    ///// Global value for chart plotting
+    // global graph variables
+    lateinit var dataSet_res_accel_x: LineDataSet
+    lateinit var dataSet_res_accel_y: LineDataSet
+    lateinit var dataSet_res_accel_z: LineDataSet
+
+    lateinit var dataSet_thingy_accel_x: LineDataSet
+    lateinit var dataSet_thingy_accel_y: LineDataSet
+    lateinit var dataSet_thingy_accel_z: LineDataSet
+
+    lateinit var allRespeckData: LineData
+
+    lateinit var allThingyData: LineData
+
+    lateinit var respeckChart: LineChart
+    lateinit var thingyChart: LineChart
+
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recognise)
 
         setupViews()
         setupButtons()
+        setupCharts()
 
         str = "Recognition result : "
         respeckWindow = Array(windowSize) {FloatArray(respeckFeatureSize)} //{respeckWindowRow}
@@ -118,9 +147,13 @@ class RecogniseActivity : AppCompatActivity() {
 
                     respeckOn = true
 
+                    // get all relevant intent contents
+                    val x = liveData.accelX
+                    val y = liveData.accelY
+                    val z = liveData.accelZ
 
                     time += 1
-
+                    updateGraph("respeck", x, y, z)
                 }
             }
         }
@@ -148,8 +181,14 @@ class RecogniseActivity : AppCompatActivity() {
 
                     updateData(liveData)
 
+                    // get all relevant intent contents
+                    val x = liveData.accelX
+                    val y = liveData.accelY
+                    val z = liveData.accelZ
+
                     thingyOn = true
                     time += 1
+                    updateGraph("thingy", x, y, z)
 
                 }
             }
@@ -573,6 +612,132 @@ class RecogniseActivity : AppCompatActivity() {
             Toast.makeText(this, "Error while saving recording!", Toast.LENGTH_SHORT).show()
             Log.e(TAG, "saveRespeckRecording: Error while writing to the respeck file: " + e.message )
         }
+    }
+
+
+
+
+    fun setupCharts() {
+        respeckChart = findViewById(R.id.respeck_chart)
+        thingyChart = findViewById(R.id.thingy_chart)
+
+        // Respeck
+
+        time = 0f
+        val entries_res_accel_x = ArrayList<Entry>()
+        val entries_res_accel_y = ArrayList<Entry>()
+        val entries_res_accel_z = ArrayList<Entry>()
+
+        dataSet_res_accel_x = LineDataSet(entries_res_accel_x, "Accel X")
+        dataSet_res_accel_y = LineDataSet(entries_res_accel_y, "Accel Y")
+        dataSet_res_accel_z = LineDataSet(entries_res_accel_z, "Accel Z")
+
+        dataSet_res_accel_x.setDrawCircles(false)
+        dataSet_res_accel_y.setDrawCircles(false)
+        dataSet_res_accel_z.setDrawCircles(false)
+
+        dataSet_res_accel_x.setColor(
+            ContextCompat.getColor(
+                this,
+                R.color.red
+            )
+        )
+        dataSet_res_accel_y.setColor(
+            ContextCompat.getColor(
+                this,
+                R.color.green
+            )
+        )
+        dataSet_res_accel_z.setColor(
+            ContextCompat.getColor(
+                this,
+                R.color.blue
+            )
+        )
+
+        val dataSetsRes = ArrayList<ILineDataSet>()
+        dataSetsRes.add(dataSet_res_accel_x)
+        dataSetsRes.add(dataSet_res_accel_y)
+        dataSetsRes.add(dataSet_res_accel_z)
+
+        allRespeckData = LineData(dataSetsRes)
+        respeckChart.data = allRespeckData
+        respeckChart.invalidate()
+
+        // Thingy
+
+        time = 0f
+        val entries_thingy_accel_x = ArrayList<Entry>()
+        val entries_thingy_accel_y = ArrayList<Entry>()
+        val entries_thingy_accel_z = ArrayList<Entry>()
+
+        dataSet_thingy_accel_x = LineDataSet(entries_thingy_accel_x, "Accel X")
+        dataSet_thingy_accel_y = LineDataSet(entries_thingy_accel_y, "Accel Y")
+        dataSet_thingy_accel_z = LineDataSet(entries_thingy_accel_z, "Accel Z")
+
+        dataSet_thingy_accel_x.setDrawCircles(false)
+        dataSet_thingy_accel_y.setDrawCircles(false)
+        dataSet_thingy_accel_z.setDrawCircles(false)
+
+        dataSet_thingy_accel_x.setColor(
+            ContextCompat.getColor(
+                this,
+                R.color.red
+            )
+        )
+        dataSet_thingy_accel_y.setColor(
+            ContextCompat.getColor(
+                this,
+                R.color.green
+            )
+        )
+        dataSet_thingy_accel_z.setColor(
+            ContextCompat.getColor(
+                this,
+                R.color.blue
+            )
+        )
+
+        val dataSetsThingy = ArrayList<ILineDataSet>()
+        dataSetsThingy.add(dataSet_thingy_accel_x)
+        dataSetsThingy.add(dataSet_thingy_accel_y)
+        dataSetsThingy.add(dataSet_thingy_accel_z)
+
+        allThingyData = LineData(dataSetsThingy)
+        thingyChart.data = allThingyData
+        thingyChart.invalidate()
+    }
+
+    fun updateGraph(graph: String, x: Float, y: Float, z: Float) {
+        // take the first element from the queue
+        // and update the graph with it
+        if (graph == "respeck") {
+            dataSet_res_accel_x.addEntry(Entry(time, x))
+            dataSet_res_accel_y.addEntry(Entry(time, y))
+            dataSet_res_accel_z.addEntry(Entry(time, z))
+
+            runOnUiThread {
+                allRespeckData.notifyDataChanged()
+                respeckChart.notifyDataSetChanged()
+                respeckChart.invalidate()
+                respeckChart.setVisibleXRangeMaximum(150f)
+                respeckChart.moveViewToX(respeckChart.lowestVisibleX + 40)
+            }
+        } else if (graph == "thingy") {
+            dataSet_thingy_accel_x.addEntry(Entry(time, x))
+            dataSet_thingy_accel_y.addEntry(Entry(time, y))
+            dataSet_thingy_accel_z.addEntry(Entry(time, z))
+
+            runOnUiThread {
+                allThingyData.notifyDataChanged()
+                thingyChart.notifyDataSetChanged()
+                thingyChart.invalidate()
+                thingyChart.setVisibleXRangeMaximum(150f)
+                thingyChart.moveViewToX(thingyChart.lowestVisibleX + 40)
+            }
+        }
+
+
     }
 
     private fun mapToString(m: MutableMap<String, Long>) : String {
